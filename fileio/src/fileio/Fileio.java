87e56b65ea6cf9bb1a4b59f9b59e3400c5971dfd
mrlupher <lupherj@uinta4.com>
2014-11-11 17:41:58 -0700

    First commit

---
+++ fileio/src/fileio/Fileio.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fileio;
+
+/**
+ *
+ * @author lupherj
+ */
+import java.util.Scanner;
+public class Fileio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Use this project to create classes and objects for file io
+        Scanner input = new Scanner(System.in);
+    }
+    
+}
